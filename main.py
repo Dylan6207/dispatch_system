6eff51dbfbaebbee324d3f01470a3afc56c7aa5b
Dylan <dylan1700.syu@gmail.com>
2025-07-27 18:06:56 +0800

    fix: SQLAlchemy re-initialization error

--- main.py
+++ main.py
@@ -15,8 +15,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-
-db.init_app(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
