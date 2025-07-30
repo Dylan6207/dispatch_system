c1fe5f7ad62e0ab82a1f113f16c829cc2be41274
Dylan <dylan1700.syu@gmail.com>
2025-07-30 17:06:16 +0800

    fix: index 405 issue

--- app/routes/index.py
+++ app/routes/index.py
@@ -1,7 +1,9 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request
 
 index_bp = Blueprint('index', __name__)
 
-@index_bp.route('/')
+@index_bp.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        return redirect(url_for('auth.login'))
+    return render_template('login.html')
