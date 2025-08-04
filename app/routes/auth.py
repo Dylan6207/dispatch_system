8f397971a97e5eaa6f53161af226a8ea6f2f1314
Dylan <dylan1700.syu@gmail.com>
2025-08-04 22:42:37 +0800

    the selection of villages for users registeration

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,8 +1,15 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request, jsonify
 from flask_login import login_user, logout_user, login_required
 from app.models import db, User
+import json
+import os
 
 auth = Blueprint("auth", __name__)
+
+# 讀取 village_data.json
+DATA_PATH = os.path.join(os.path.dirname(__file__), "../village_data.json")
+with open(DATA_PATH, encoding='utf-8') as f:
+    village_data = json.load(f)
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
@@ -49,32 +56,18 @@ def register():
 
 @auth.route('/api/counties')
 def get_counties():
-    counties = ["台北市", "新北市"]
+    counties = list(village_data.keys())
     return jsonify(counties)
 
 @auth.route('/api/towns')
 def get_towns():
     county = request.args.get('county')
-    towns_data = {
-        "台北市": ["中正區", "大安區"],
-        "新北市": ["板橋區", "新莊區"]
-    }
-    towns = towns_data.get(county, [])
+    towns = list(village_data.get(county, {}).keys())
     return jsonify(towns)
 
 @auth.route('/api/villages')
 def get_villages():
     county = request.args.get('county')
     town = request.args.get('town')
-    villages_data = {
-        "台北市": {
-            "中正區": ["仁愛里", "幸福里"],
-            "大安區": ["和平里", "新興里"]
-        },
-        "新北市": {
-            "板橋區": ["民生里", "新興里"],
-            "新莊區": ["幸福里", "仁愛里"]
-        }
-    }
-    villages = villages_data.get(county, {}).get(town, [])
+    villages = village_data.get(county, {}).get(town, [])
     return jsonify(villages)
